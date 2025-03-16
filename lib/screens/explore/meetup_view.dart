6b2456f18d2f972fb9d7cc100d3b5b8f96d4b8e2
Nabil Lemenuel <nabil.lemenuel@gmail.com>
2025-03-16 20:13:05 +0100

    Remove unused package

--- lib/screens/explore/meetup_view.dart
+++ lib/screens/explore/meetup_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
-import 'dart:ui' as ui;
 import 'package:url_launcher/url_launcher.dart';
 import 'package:intl/intl.dart';
 import '../../constants/app_theme.dart';
